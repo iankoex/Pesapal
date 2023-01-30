5e49cfc12af317ea5854034154f89fdc3c922b35
Iankoex <iankoex@icloud.com>
2023-01-30 20:04:17 +0300

    Initial Commit

---
+++ Pesapal/PesapalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PesapalApp.swift
+//  Pesapal
+//
+//  Created by Ian on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PesapalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
